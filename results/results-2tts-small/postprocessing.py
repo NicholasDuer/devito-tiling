6ac107b399ee1dad4ff2d3610995f3b50477665a
NicholasDuer <nicholas_duer@yahoo.co.uk>
2023-04-24 19:00:40 +0100

    Add line graph for comm times

--- results/results-2tts-small/postprocessing.py
+++ results/results-2tts-small/postprocessing.py
@@ -1,15 +1,17 @@
 import csv 
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import pandas as pd
+import numpy as np
 
 space_orders = [2, 4, 8]
-tts = 2
+tts = 8
 num_repeats = 3
 
 results_folder = "results/"
 graphs_folder = "graphs/"
 
-def generate_graph(space_order):
+def generate_bar_graph(space_order):
     file_path_standard = results_folder + "results_standard_mpi_" + str(space_order) + "so.csv"
     file_path_overlapped = results_folder + "results_overlapped_mpi_" + str(space_order) + "so.csv"
     file_path_openmp = results_folder + "results_openmp_" + str(space_order) + "so.csv" 
@@ -85,7 +87,7 @@ def generate_graph(space_order):
         compute_results.append([experiment_name, get_avg_time(overlapped_compute), get_avg_time(standard_compute)])
 
     def save_graph(results):
-        title = "Small Laplace Experiments, TTS: " + str(tts) + ", SO: " + str(space_order)
+        title = "Laplace Experiments, TTS: " + str(tts) + ", SO: " + str(space_order)
         df = pd.DataFrame(results, columns=['Dimensions', 'Overlapped Tiling MPI', 'Standard MPI', 'OpenMP'])
         df.plot(x='Dimensions', kind='bar', rot=10, ylabel="Time elapsed (s)", 
                 title="Elapsed Times, " + title)
@@ -102,8 +104,62 @@ def generate_graph(space_order):
         df = pd.DataFrame(compute_results, columns=['Dimensions', 'Overlapped Tiling MPI', 'Standard MPI'])
         df.plot(x='Dimensions', kind='bar', rot=10, ylabel="Time spent on floating point operations (s) ", title="Floating Point Operation Times, " + title)
         plt.savefig(graphs_folder + "flop_times_" + str(space_order) + "so")
-    
+
     save_graph(results)
 
+def generate_line_graph():
+    fig, ax = plt.subplots()
+    ax.set_title("Communication Times, Laplace Experiments, TTS: " + str(tts))
+    ax.set_xlabel("Time spent on communcation, standard MPI (s)")
+    ax.set_ylabel("Time spent on communcation, overlapped tiling MPI (s)")
+
+    all_standard_comm_times = []
+    all_overlapped_comm_times = []
+
+    colours = ['blue', 'green', 'darkorange']
+    handles = []
+
+    for space_order in space_orders:
+        file_path_standard = results_folder + "results_standard_mpi_" + str(space_order) + "so.csv"
+        file_path_overlapped = results_folder + "results_overlapped_mpi_" + str(space_order) + "so.csv"
+
+        standard_csv = csv.reader(open(file_path_standard))
+        overlapped_csv = csv.reader(open(file_path_overlapped))
+
+        headers = next(standard_csv)
+        _ = next(overlapped_csv)
+
+        standard_comm_times = []
+        overlapped_comm_times = []
+
+        for standard_row in standard_csv:
+            overlapped_row = next(overlapped_csv)
+            for i in range(1, num_repeats):
+                try:
+                    halo = float(standard_row[headers.index("haloupdate0")])
+                    standard_comm_times.append(halo)
+                    all_standard_comm_times.append(halo)
+                except IndexError:
+                    pass
+                try:
+                    halo = float(overlapped_row[headers.index("haloupdate0")])
+                    overlapped_comm_times.append(halo)
+                    all_overlapped_comm_times.append(halo)
+                except IndexError:
+                    pass
+        colour = colours[space_orders.index(space_order)]    
+        plt.scatter(x=standard_comm_times, y=overlapped_comm_times, color=colour)  
+        handles.append(mpatches.Patch(color=colour, label="Space Order " + str(space_order)))
+   
+    m, c = np.polyfit(all_standard_comm_times, all_overlapped_comm_times, 1)
+    plt.plot(all_standard_comm_times, np.array(all_standard_comm_times) * m + c, color='red')
+    plt.text(all_standard_comm_times[10], all_overlapped_comm_times[-4], 'y = ' + str(round(m, ndigits=2)) + "x + " + str(round(c, ndigits=2)), size=10, weight="bold")  
+    plt.legend(handles=handles, loc='lower right')
+    plt.savefig(graphs_folder + "comm_times_line")
+
+
 for space_order in space_orders:
-    generate_graph(space_order)
+    generate_bar_graph(space_order)
+
+
+generate_line_graph()
